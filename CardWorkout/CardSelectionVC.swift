5c675ba20ab4348b0e3c37f765d77d6610d238e3
Ege Kaygisizer <ekaygisizer@gmail.com>
2024-12-02 18:54:09 +0300

    Kapama düğmesi eklemeden önce.

---
+++ CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,35 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by Ege Kaygisizer on 2/12/24.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        for button in buttons {
+            button.layer.cornerRadius = 8
+        }        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func restartButtonTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func rulesButtonTapped(_ sender: Any) {
+    }
+
+
+}

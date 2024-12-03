845515022f1beebe33cdc575a7781bc7520bb510
Ege Kaygisizer <ekaygisizer@gmail.com>
2024-12-03 11:11:35 +0300

    its all working, adding start button to stop button

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -12,26 +12,45 @@ class CardSelectionVC: UIViewController {
     @IBOutlet weak var cardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
     
+    var timer : Timer!
+    var card : [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         for button in buttons {
             button.layer.cornerRadius = 8
-        }        // Do any additional setup after loading the view.
+        }
+        
+        let deck = Decks()  // Decks nesnesi oluşturuluyor
+        card = deck.AllCards
     }
     
+    
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector (updateCards), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCards() {
+        cardImageView.image = card.randomElement() ?? UIImage(named: "AS")
+        }
+    
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer?.invalidate()
+        timer = nil
     }
     
     
     @IBAction func restartButtonTapped(_ sender: Any) {
+        timer?.invalidate()
+        timer = nil
+        startTimer()
     }
     
     
     @IBAction func rulesButtonTapped(_ sender: Any) {
     }
-    
-    
     @IBAction func rulesCloseButton(unwindSegue: UIStoryboardSegue){
         
     }
